35311aa9c28b1bc39367d62594194ebb715ee2a2
Steeven Maila <steevenandresmaila@gmail.com>
2026-03-16 09:35:39 -0500

    Se actualiza ruta de backend apuntando al backend alojado en vps aromotor: constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-export const API_BASE_URL = "https://aromotor-backend.onrender.com/api/"
+export const API_BASE_URL = "https://steven-2026-backend-estado-cuenta.c9r1sk.easypanel.host/api/"
+// export const API_BASE_URL = "https://aromotor-backend.onrender.com/api/"
 // export const API_BASE_URL = "http://127.0.0.1:8000/api/"
 // export const API_BASE_URL = "https://estadocuenta-2f2j.onrender.com/api/"
